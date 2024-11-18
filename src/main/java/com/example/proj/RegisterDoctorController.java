a25d2d5b444a3282177c5a8eba9d7632853ef8f0
MTalhaAli47 <65600186+MTalhaAli47@users.noreply.github.com>
2024-11-18 23:56:01 +0500

    staffRegsiterButton linked

--- src/main/java/com/example/proj/RegisterDoctorController.java
+++ src/main/java/com/example/proj/RegisterDoctorController.java
@@ -15,6 +15,7 @@ public class RegisterDoctorController {
     public Button docRegCancel;
     public Button doctoralready;
     @FXML
+    public Button docRegButton;
     private Label welcomeText;
 
     @FXML
@@ -49,6 +50,26 @@ public class RegisterDoctorController {
             Parent newPage = loader.load();
 
             Stage currentStage = (Stage)doctoralready .getScene().getWindow();
+
+            // Create a new stage
+
+            currentStage.setScene(new Scene(newPage));
+            currentStage.setTitle("Doctor Login");
+            currentStage.sizeToScene();
+            currentStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Debugging in case of issues loading the FXML
+        }
+    }
+
+    public void HandleDocReg(ActionEvent actionEvent) {
+        try {
+            // Load the FXML for the About Us application
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("DoctorLogin.fxml")); // Ensure AboutUs.fxml exists in the same directory
+            Parent newPage = loader.load();
+
+            Stage currentStage = (Stage)docRegButton .getScene().getWindow();
 
             // Create a new stage
 
