a0150e06dae465d3079c2a48d65e211692f4e093
Ayna <i221054@nu.edu.pk>
2024-11-18 23:04:55 +0500

    linked patient log in page to home page

--- src/main/java/com/example/proj/PatientLoginController.java
+++ src/main/java/com/example/proj/PatientLoginController.java
@@ -1,6 +1,7 @@
 package com.example.proj;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,6 +12,8 @@ import java.io.IOException;
 
 public class PatientLoginController {
     public Button hello2;
+@FXML
+     private Button patientlog;
     public void patientgoback(ActionEvent actionEvent) {
         try {
             // Load the FXML for the About Us application
@@ -29,5 +32,24 @@ public class PatientLoginController {
         } catch (IOException e) {
             e.printStackTrace(); // Debugging in case of issues loading the FXML
         }
+    }
+
+    public void HandlePatientLog(ActionEvent actionEvent) {try {
+        // Load the FXML for the About Us application
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PatientHome.fxml")); // Ensure AboutUs.fxml exists in the same directory
+        Parent newPage = loader.load();
+
+        Stage currentStage = (Stage) patientlog.getScene().getWindow();
+
+        // Create a new stage
+
+        currentStage.setScene(new Scene(newPage));
+        currentStage.setTitle("Patient Home Page");
+        currentStage.sizeToScene();
+        currentStage.show();
+
+    } catch (IOException e) {
+        e.printStackTrace(); // Debugging in case of issues loading the FXML
+    }
     }
 }
