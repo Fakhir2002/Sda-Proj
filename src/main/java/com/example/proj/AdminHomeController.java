ea288d4dd37a4270d6cc07d87a84926efdf61ae9
MTalhaAli47 <65600186+MTalhaAli47@users.noreply.github.com>
2024-11-20 23:24:08 +0500

    AdminHome created and linked with AdminLogin

---
+++ src/main/java/com/example/proj/AdminHomeController.java
@@ -0,0 +1,39 @@
+package com.example.proj;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class AdminHomeController {
+
+
+    @FXML
+    private Button Adminlogout;
+
+    public void handleAdminlogout(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
+            Parent newPage = loader.load();
+
+            Stage currentStage = (Stage) Adminlogout.getScene().getWindow();
+
+            // Create a new stage
+
+            currentStage.setScene(new Scene(newPage));
+            currentStage.setTitle("Home Page");
+            currentStage.sizeToScene();
+            currentStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Debugging in case of issues loading the FXML
+        }
+    }
+}
