71e68e3b5abf7c87a3d9c22b43491d282eda9e04
MTalhaAli47 <65600186+MTalhaAli47@users.noreply.github.com>
2024-11-20 21:47:37 +0500

    yaad ni

--- src/main/java/com/example/proj/AdminLoginController.java
+++ src/main/java/com/example/proj/AdminLoginController.java
@@ -1,6 +1,7 @@
 package com.example.proj;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,6 +12,8 @@ import java.io.IOException;
 
 public class AdminLoginController {
     public Button hello;
+    @FXML
+    private Button AdminLogin;
     public void adminback(ActionEvent actionEvent) {
         try {
             // Load the FXML for the About Us application
@@ -23,6 +26,26 @@ public class AdminLoginController {
 
             currentStage.setScene(new Scene(newPage));
             currentStage.setTitle("Home Page");
+            currentStage.sizeToScene();
+            currentStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Debugging in case of issues loading the FXML
+        }
+    }
+
+    public void handleAdminLogin(ActionEvent actionEvent) {
+        try {
+            // Load the FXML for the About Us application
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminHome.fxml")); // Ensure AboutUs.fxml exists in the same directory
+            Parent newPage = loader.load();
+
+            Stage currentStage = (Stage) AdminLogin.getScene().getWindow();
+
+            // Create a new stage
+
+            currentStage.setScene(new Scene(newPage));
+            currentStage.setTitle("Admin Home Page");
             currentStage.sizeToScene();
             currentStage.show();
 
